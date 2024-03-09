dfb3771bef3e2df46b2efa52e578f4e923e684b9
calebbestes <131561201+calebbestes@users.noreply.github.com>
2024-03-08 20:03:49 -0700

    Cool typing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
 import styles from "~/styles/main.css";
-import { Button } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,33 +19,24 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Zork-AI</h1>
       <Button variant="contained">Contained</Button>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Card style={{ maxWidth: 345 }}>
+        <CardActionArea component={Link} to="/new-page">
+          <CardMedia
+            component="img"
+            height="140"
+            image="~/lib/36E58462-0C44-47B3-BB3B-6C9F2E48379A.jpeg"
+            alt="Play"
+          />
+          <Typography gutterBottom variant="h5" component="div">
+            Sci-Fi
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Conquer the cosmos. Are you up for the challenge?
+          </Typography>
+        </CardActionArea>
+      </Card>
     </div>
   );
 }
